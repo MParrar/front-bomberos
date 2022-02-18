0625a0f5a4fa685e04dc4c41b29ec4a0c561d494
Fabian <fabian.ecm96@gmail.com>
2022-02-18 16:08:37 -0300

    fixed baseUrl

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
   // baseURL: process.env.REACT_APP_BACKEND_URL
-  baseURL: 'https://api-bomberos-pinto.herokuapp.com/',
+  baseURL: 'https://api-bomberos-pinto.herokuapp.com/api',
 });
 
 export default clienteAxios;
