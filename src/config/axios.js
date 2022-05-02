12202344aac3e5763b3c315007e5e4e2b75eab27
Matias Parra <parramatias96@gmail.com>
2022-05-02 17:49:24 -0400

    cambio ambiente

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
   // baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: 'https://api-bomberos-pinto.herokuapp.com/api',
+  // baseURL: 'https://api-bomberos-pinto.herokuapp.com/api',
+  baseURL: 'https://api-bomberos-production-0818.up.railway.app/api'
 });
 
 export default clienteAxios;
